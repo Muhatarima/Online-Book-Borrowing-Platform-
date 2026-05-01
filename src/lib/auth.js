262999bdb2efe2a1308c357d2169f316b645aeb9
Muhatarima <muhatarima@gmail.com>
2026-05-01 19:26:12 +0600

    mongo db connected

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,10 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { client } from "@/db"; // your mongodb client
 
 export const auth = betterAuth({
-  //...
+          emailAndPassword: { 
+    enabled: true, 
+  },
+    database: mongodbAdapter(client),
 });
